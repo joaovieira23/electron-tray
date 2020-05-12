3e8c369d4b1fb4a54309cfd2c4109578ce0e6e67
joaovieira23 <joaovictorvieira.23@hotmail.com>
2020-05-12 01:25:39 -0300

    feat: add showOpenDialog

--- main.js
+++ main.js
@@ -1,13 +1,15 @@
 const { resolve } = require('path');
-const { app, Menu, Tray } = require('electron');
-
+const { app, Menu, Tray, dialog } = require('electron');
 // app.dock.hide();
 
 app.on('ready', () => {
   const tray =  new Tray(resolve(__dirname, 'assets', 'iconTemplate.png'));
 
   const contextMenu = Menu.buildFromTemplate([
-    { label: 'Item1', type: 'radio', checked: true }
+    { label: 'Item1', type: 'radio', checked: true, click: () => {
+      dialog.showOpenDialog({ properties: ['openDirectory'] })
+      .then(paths => console.log(paths))
+    }}
   ]);
 
   tray.setContextMenu(contextMenu);
