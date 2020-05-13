aec1f365c3eb98f90a853e337e719ead86a16eb8
joaovieira23 <joaovictorvieira.23@hotmail.com>
2020-05-13 13:53:31 -0300

    feat: add functionality to remove projects and close the application

--- main.js
+++ main.js
@@ -1,6 +1,6 @@
 const { resolve, basename } = require('path');
 const {
-  app, Menu, Tray, dialog, MenuItem,
+  app, Menu, Tray, dialog,
 } = require('electron');
 const spawn = require('cross-spawn');
 const Store = require('electron-store');
@@ -19,41 +19,62 @@ const store = new Store({ schema });
 let tray = null;
 
 function render() {
-  if (!tray.isDestroyed()) {
-    tray.destroy();
-    tray = new Tray(resolve(__dirname, 'assets', 'iconTemplate.png'));
-  }
+  // if (!tray.isDestroyed()) {
+  //   tray.destroy();
+  //   tray = new Tray(resolve(__dirname, 'assets', 'iconTemplate.png'));
+  // }
 
   const storedProjects = store.get('projects');
   const projects = storedProjects ? JSON.parse(storedProjects) : [];
   // store.clear();
 
-  const items = projects.map((project) => ({
-    label: project.name,
-    click: () => {
-      spawn('code', [project.path], { stdio: 'inherit' });
-      // Menu.getApplicationMenu().append(new MenuItem({ label: 'Agora' }));
-    },
+  const items = projects.map(({ name, path }) => ({
+    label: name,
+    submenu: [
+      {
+        label: 'Abrir no VSCode',
+        click: () => {
+          spawn('code', [path], { stdio: 'inherit' });
+        },
+      },
+      {
+        label: 'Remover',
+        click: () => {
+          store.set('projects', JSON.stringify(projects.filter((item) => item.path !== path)));
+          render();
+        },
+      },
+    ],
+
+    // Menu.getApplicationMenu().append(new MenuItem({ label: 'Agora' }));
   }));
 
   const contextMenu = Menu.buildFromTemplate([
-    ...items,
-    // {
-    //   type: 'separator',
-    // },
-  ]);
-
-  contextMenu.insert(0, new MenuItem({
-    label: 'Adicionar novo projeto...',
-    click: () => {
-      dialog.showOpenDialog({ properties: ['openDirectory'] })
-        .then((file) => store.set('projects', JSON.stringify([...projects, {
-          path: file.filePaths.join(),
-          name: basename(file.filePaths.join()),
-        }])));
-      render();
+    {
+      label: 'Adicionar novo projeto...',
+      click: () => {
+        dialog.showOpenDialog({ properties: ['openDirectory'] })
+          .then((file) => store.set('projects', JSON.stringify([...projects, {
+            path: file.filePaths.join(),
+            name: basename(file.filePaths.join()),
+          }])));
+        render();
+      },
     },
-  }));
+    {
+      type: 'separator',
+    },
+    ...items,
+    {
+      type: 'separator',
+    },
+    {
+      type: 'normal',
+      label: 'Fechar Code Tray',
+      role: 'quit',
+      enabled: true,
+    },
+  ]);
 
   tray.setContextMenu(contextMenu);
 }
