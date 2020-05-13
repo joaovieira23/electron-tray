b31eb56a97d1197f72ae082f73f938d20ce27beb
joaovieira23 <joaovictorvieira.23@hotmail.com>
2020-05-13 01:16:57 -0300

    feat: open projects in visual studio code

--- main.js
+++ main.js
@@ -1,18 +1,62 @@
-const { resolve } = require('path');
-const { app, Menu, Tray, dialog } = require('electron');
-// app.dock.hide();
+const { resolve, basename } = require('path');
+const {
+  app, Menu, Tray, dialog, MenuItem,
+} = require('electron');
+const Store = require('electron-store');
+const spawn = require('cross-spawn');
 
-app.on('ready', () => {
-  const tray =  new Tray(resolve(__dirname, 'assets', 'iconTemplate.png'));
+const schema = {
+  projects: {
+    type: 'string',
+  },
+};
+
+const store = new Store({ schema });
+
+let tray = null;
+
+function render() {
+  if (!tray.isDestroyed()) {
+    tray.destroy();
+    tray = new Tray(resolve(__dirname, 'assets', 'iconTemplate.png'));
+  }
+
+  const storedProjects = store.get('projects');
+  const projects = storedProjects ? JSON.parse(storedProjects) : [];
+  // store.clear();
+
+  const items = projects.map((project) => ({
+    label: project.name,
+    click: () => {
+      spawn('code', [project.path], { stdio: 'inherit' });
+      // Menu.getApplicationMenu().append(new MenuItem({ label: 'Agora' }));
+    },
+  }));
 
   const contextMenu = Menu.buildFromTemplate([
-    { label: 'Item1', type: 'radio', checked: true, click: () => {
-      dialog.showOpenDialog({ properties: ['openDirectory'] })
-      .then(paths => console.log(paths))
-    }}
+    ...items,
+    // {
+    //   type: 'separator',
+    // },
   ]);
 
+  contextMenu.insert(0, new MenuItem({
+    label: 'Adicionar novo projeto...',
+    click: () => {
+      dialog.showOpenDialog({ properties: ['openDirectory'] })
+        .then((file) => store.set('projects', JSON.stringify([...projects, {
+          path: file.filePaths.join(),
+          name: basename(file.filePaths.join()),
+        }])));
+      render();
+    },
+  }));
+
   tray.setContextMenu(contextMenu);
+}
+
+app.on('ready', () => {
+  tray = new Tray(resolve(__dirname, 'assets', 'iconTemplate.png'));
+
+  render();
 });
-
-
